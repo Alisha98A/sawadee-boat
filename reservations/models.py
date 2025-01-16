59dffc2a654b3144b4eaf8b94fe7f8e5daa73e26
Alisha <alishaanderssonn@icloud.com>
2025-01-16 14:18:11 +0100

    Add boat model to models.py and register it to admin.py

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Boat(models.Model):
+    """
+    The Boat model represents a dining boat used for reservations.
+    It stores information about the boat's name, description, and capacity.
+    This model is used to keep track of available boats for customer bookings.
+    """
+    name = models.CharField(max_length=255)
+    description = models.TextField()  
+    capacity = models.IntegerField()  
+
+    def __str__(self):
+        return self.name
