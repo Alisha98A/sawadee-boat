f8aeb1ada2d37c152f11a1ce37332c009de2238a
Alisha <alishaanderssonn@icloud.com>
2025-01-16 14:32:50 +0100

    Add Reservation model in models.py, register in admin.py

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Boat
+from .models import Boat, Reservation
 
 # Register your models here.
 admin.site.register(Boat)
+admin.site.register(Reservation)
--- reservations/models.py
+++ reservations/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 # Create your models here.
@@ -15,3 +17,26 @@ class Boat(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class Reservation(models.Model):
+    """
+    The Reservation model represents a booking made by a user for a specific boat.
+    It stores the user who made the reservation, the boat being reserved, the booking date, 
+    the time slot, and the number of guests. It also includes an optional discount field.
+    The model ensures that the number of guests is between 4 and 20 through the clean method.
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  
+    boat = models.ForeignKey(Boat, on_delete=models.CASCADE)  
+    booking_date = models.DateField()  
+    time_slot = models.TimeField()  
+    number_of_guests = models.IntegerField() 
+    has_discount = models.BooleanField(default=False, null=True, blank=True) 
+
+    def __str__(self):
+        return f"Reservation for {self.user.username} on {self.booking_date}"
+
+    # Ensures that number of guests is within the specified range (4-20)
+    def clean(self):
+        if not 4 <= self.number_of_guests <= 20:
+            raise ValidationError('Number of guests must be between 4 and 20.')
