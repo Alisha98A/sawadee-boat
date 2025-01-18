6c7a2bd19ad5aa2a0037a25131df0d00d9a60c04
Alisha Andersson <alishaanderssonn@icloud.com>
2025-01-18 19:04:40 +0000

    Customize admin interface for Reservation model

--- reservations/admin.py
+++ reservations/admin.py
@@ -9,4 +9,13 @@ class BoatAdmin(admin.ModelAdmin):
     list_display = ('name', 'capacity')
     search_fields = ('name',)
 
-admin.site.register(Reservation)
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    """
+    Customize the admin interface for the Reservation model.
+    """
+    list_display = ('user', 'user_email', 'boat', 'booking_date', 'time_slot', 'number_of_guests', 'has_discount')
+    list_filter = ('booking_date', 'boat')
+    search_fields = ('user__username', 'boat__name', 'user__email')  
+    ordering = ('booking_date', 'time_slot')
+    date_hierarchy = 'booking_date'
