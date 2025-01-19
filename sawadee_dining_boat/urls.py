ff34b0450200ece8a230db3d188e1e19f0fc41b2
Alisha Andersson <alishaanderssonn@icloud.com>
2025-01-19 12:35:56 +0000

    Include info app URLs in the project URLs

--- sawadee_dining_boat/urls.py
+++ sawadee_dining_boat/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 from reservations import views as index_views
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('', include('info.urls')),  
 ]
