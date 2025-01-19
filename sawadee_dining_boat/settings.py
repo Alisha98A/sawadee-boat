308c42e683259f17ccb0445fc8ad83071729346e
Alisha Andersson <alishaanderssonn@icloud.com>
2025-01-19 12:37:40 +0000

    Update TEMPLATES DIRS to include project-level templates

--- sawadee_dining_boat/settings.py
+++ sawadee_dining_boat/settings.py
@@ -60,7 +60,9 @@ ROOT_URLCONF = 'sawadee_dining_boat.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
