c633f52c409d6ba8e5dc7f10b432f17ff32d381e
Alisha Andersson <alishaanderssonn@icloud.com>
2025-01-22 22:58:23 +0000

    Add path to profile in account app's urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path, include
+from . import views
+from .views import profile_view, ProfileUpdateView
+from django.views.generic import TemplateView 
+
 
 urlpatterns = [
-    path('accounts/', include('allauth.urls')),
+    path('profile/', views.profile_view, name='profile'),
+    path('profile/edit/', ProfileUpdateView.as_view(), name='edit_profile'), 
 ]
