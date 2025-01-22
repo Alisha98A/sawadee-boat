e157f1533460392899a5b784bb5c971206336ab2
Alisha Andersson <alishaanderssonn@icloud.com>
2025-01-22 18:11:57 +0000

    Add Profile model to store user information

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+ #Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    address = models.TextField(max_length=255, blank=True)
+    phone_number = models.CharField(max_length=15, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
