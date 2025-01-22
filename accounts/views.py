5b8f8f12cff36f93bdde1c501523e57035e38e91
Alisha Andersson <alishaanderssonn@icloud.com>
2025-01-22 22:50:11 +0000

    Add profile view logic

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 # Create your views here.
+
+# Profile view
+@login_required
+def profile_view(request):
+    profile, created = Profile.objects.get_or_create(user=request.user)
+    return render(request, 'accounts/profile.html', {'profile': profile})
