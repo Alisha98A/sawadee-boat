e82a9f1b16a01d596adf2f240bff52a4d5512040
Alisha <alishaanderssonn@icloud.com>
2025-01-28 22:47:23 +0100

    Remove BoatAdmin class and add extra fields in list display

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,22 +1,15 @@
 from django.contrib import admin
-from .models import Boat, Reservation
-
-@admin.register(Boat)
-class BoatAdmin(admin.ModelAdmin):
-    """
-    Customize the admin interface for the Boat model.
-    """
-    list_display = ('name', 'capacity')
-    search_fields = ('name',)
+from .models import Reservation
 
 @admin.register(Reservation)
 class ReservationAdmin(admin.ModelAdmin):
     """
     Customize the admin interface for the Reservation model.
     """
-    list_display = ('user', 'user_email', 'boat', 'booking_date', 'time_slot', 'number_of_guests', 'has_discount')
-    list_filter = ('booking_date', 'boat')
-    search_fields = ('user__username', 'boat__name', 'user__email')  # Add email to search fields
+    list_display = ('user', 'first_name', 'last_name', 'email_address', 'phone_number', 
+                    'booking_date', 'time_slot', 'number_of_guests')
+    list_filter = ('booking_date',)
+    search_fields = ('user__username', 'first_name', 'last_name', 'email_address', 'phone_number')  
     ordering = ('booking_date', 'time_slot')
     date_hierarchy = 'booking_date'
 
