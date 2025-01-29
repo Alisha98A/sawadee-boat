86a8030eab6ec5c4aca9985bb467828e11e2fcc6
Alisha <alishaanderssonn@icloud.com>
2025-01-29 23:26:30 +0100

    Add namespace to reservations URLs

--- sawadee_dining_boat/urls.py
+++ sawadee_dining_boat/urls.py
@@ -5,7 +5,7 @@ from reservations import views as index_views
 urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('allauth.urls')),
-    path('reservations/', include('reservations.urls')),
+    path('reservations/', include('reservations.urls', namespace='reservations')),
     path('', include('info.urls')), 
     path('admin/', admin.site.urls),
 ]
