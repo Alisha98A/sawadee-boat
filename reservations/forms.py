fb70f2d289dadd21b57a800a5441335a94b5d3f9
Alisha <alishaanderssonn@icloud.com>
2025-01-29 22:39:49 +0100

    Create BaseReservationForm class

--- reservations/forms.py
+++ reservations/forms.py
@@ -2,3 +2,9 @@ from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from .models import Reservation
+
+class BaseReservationForm(forms.ModelForm):
+    """
+    Base reservation form containing common fields for both users and staff.
+    """
+    pass
