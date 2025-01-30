147d9b76710162703deef6f8f7cb4f4329100092
Alisha <alishaanderssonn@icloud.com>
2025-01-30 04:21:07 +0100

    Add inline editing for MenuItem in admin

--- info/admin.py
+++ info/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Menu, MenuItem, Item
 
+class MenuItemInline(admin.TabularInline):
+    """Allows MenuItems to be edited directly inside the Menu admin page."""
+    model = MenuItem
+    extra = 1
+
 admin.site.register(Menu)
 admin.site.register(MenuItem)
 admin.site.register(Item)
