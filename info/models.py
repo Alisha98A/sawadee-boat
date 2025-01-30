0044c1e1b7f3b55f719c5b5b8fcd5fa5850ef386
Alisha <alishaanderssonn@icloud.com>
2025-01-30 03:00:08 +0100

    Add Menu Model & class Meta

--- info/models.py
+++ info/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# -------------------------------------
+# Menu Model
+# -------------------------------------
+class Menu(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Menus"
