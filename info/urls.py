d1186f0e15c34257baa2552db1b1635550820fc6
Alisha <alishaanderssonn@icloud.com>
2025-01-30 04:09:14 +0100

    Add menu to urls and import menu_view

--- info/urls.py
+++ info/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .views import menu_view
+
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path("menu/", menu_view, name="menu"),
 ]
