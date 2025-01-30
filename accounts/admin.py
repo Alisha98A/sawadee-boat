790d48c48b029bc09bc820f859e39a98791281c0
Alisha <alishaanderssonn@icloud.com>
2025-01-30 23:12:46 +0100

    Set new users as inactive until confirmation

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
+from allauth.account.utils import send_email_confirmation
 from .models import Profile
 
 class CustomUserAdmin(UserAdmin):
     add_fieldsets = (
         *UserAdmin.add_fieldsets,
-        (None, {"fields": ("email",)}),  # Ensure email is required when creating a user
+        (None, {"fields": ("email",)}),
     )
+
+    def save_model(self, request, obj, form, change):
+        is_new_user = obj._state.adding  
+        super().save_model(request, obj, form, change)
+        if is_new_user:
+            obj.is_active = False  
+            obj.save()
+            send_email_confirmation(request, obj)  
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
