b21c91bc3dab7219acd4d73a07d231218fb32d4b
Alisha <alishaanderssonn@icloud.com>
2025-01-30 23:19:18 +0100

    Automatically activate user after email confirmation

--- accounts/signals.py
+++ accounts/signals.py
@@ -7,3 +7,10 @@ from .models import Profile
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.get_or_create(user=instance)
+
+@receiver(email_confirmed)
+def activate_user(sender, request, email_address, **kwargs):
+    user = email_address.user
+    if not user.is_active:
+        user.is_active = True
+        user.save()
