e706a02e6b87905079ca9b7267fc687c4f5178f1
Alisha <alishaanderssonn@icloud.com>
2025-01-30 23:22:44 +0100

    Import Allauth email confirmation signal

--- accounts/signals.py
+++ accounts/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Profile
+from allauth.account.signals import email_confirmed 
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
