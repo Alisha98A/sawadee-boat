c30ac060af9ac95c0c4c96e736a6f91fc55031dd
Alisha <alishaanderssonn@icloud.com>
2025-01-30 03:13:30 +0100

    Add urls for delete account template

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from . import views
-from .views import profile_view, ProfileUpdateView
+from .views import profile_view, ProfileUpdateView, delete_account
 from django.views.generic import TemplateView 
 
 
 urlpatterns = [
     path('profile/', views.profile_view, name='profile'),
     path('profile/edit/', ProfileUpdateView.as_view(), name='edit_profile'), 
+    path("delete-account/", delete_account, name="delete_account"),
 ]
