db39cd9aff7b80a75c686646acac52489ec95890
Alisha <alishaanderssonn@icloud.com>
2025-01-30 22:07:34 +0100

    Extend UserAdmin to require email on user creation

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,7 +3,11 @@ from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 from .models import Profile
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    add_fieldsets = (
+        *UserAdmin.add_fieldsets,
+        (None, {"fields": ("email",)}),  # Ensure email is required when creating a user
+    )
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
