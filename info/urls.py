2bff1aa9453a8c1b0b53728032cc7568623e6e66
Alisha <alishaanderssonn@icloud.com>
2025-01-31 17:33:23 +0100

    Set up url for set_sail template and import from views.py

--- info/urls.py
+++ info/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import menu_view
+from .views import menu_view, set_sail_view
 
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path("menu/", menu_view, name="menu"),
+    path("set-sail/", set_sail_view, name="set_sail"),
 ]
