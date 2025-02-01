866f3ec38eb4beeefa50dd181904f1bc342dd9bc
Alisha <alishaanderssonn@icloud.com>
2025-02-01 14:19:32 +0100

    Implement ItemForm with image validation

--- info/forms.py
+++ info/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Menu, MenuItem
+from .models import Menu, MenuItem, Item
+
 
 # -------------------------------------
 # Forms for Menu Management
@@ -13,3 +14,11 @@ class MenuItemForm(forms.ModelForm):
     class Meta:
         model = MenuItem
         fields = ["menu", "category"]
+
+class ItemForm(forms.ModelForm):
+    """Form for adding/editing menu items with image validation."""
+    image = forms.ImageField(validators=[validate_image])
+
+    class Meta:
+        model = Item
+        fields = ["menu_item", "name", "description", "price", "image"]
