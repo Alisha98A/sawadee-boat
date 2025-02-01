901987c4bcbacb2ec3c803cd84a30f8a3f77d52a
Alisha <alishaanderssonn@icloud.com>
2025-02-01 14:18:07 +0100

    Implement MenuItemForm to manage menu-category relationships

--- info/forms.py
+++ info/forms.py
@@ -1,6 +1,5 @@
 from django import forms
-from .models import Menu
-
+from .models import Menu, MenuItem
 
 # -------------------------------------
 # Forms for Menu Management
@@ -9,3 +8,8 @@ class MenuForm(forms.ModelForm):
     class Meta:
         model = Menu
         fields = ["name", "description", "is_active"]
+
+class MenuItemForm(forms.ModelForm):
+    class Meta:
+        model = MenuItem
+        fields = ["menu", "category"]
