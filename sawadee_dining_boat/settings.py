1cc88b632b5c9c96f20a2d73b578818d640954d4
Alisha <alishaanderssonn@icloud.com>
2025-02-01 20:27:42 +0100

    Rearrange STATICFILES structure in settings.py

--- sawadee_dining_boat/settings.py
+++ sawadee_dining_boat/settings.py
@@ -174,11 +174,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-WHITENOISE_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Limit maximum file upload size (Django-wide)
 DATA_UPLOAD_MAX_MEMORY_SIZE = 2 * 1024 * 1024
