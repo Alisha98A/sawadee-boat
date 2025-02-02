2b9164f7568fefb13d62944f76c1f94ca0bb7685
Alisha <alishaanderssonn@icloud.com>
2025-02-02 19:23:53 +0100

    Add unit tests for the rest of the URLs in info app

--- info/tests/test_urls.py
+++ info/tests/test_urls.py
@@ -38,3 +38,69 @@ class TestUrls(TestCase):
         """Testing the set sail page url"""
         response = self.client.get("/set-sail/")
         self.assertEqual(response.status_code, 200)
+
+    def test_staff_menu_page(self):
+        """Testing the staff menu page url"""
+        response = self.client.get("/staff/menu/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_staff_menu_page_logout(self):
+        """Testing the staff menu logout url"""
+        self.client.logout()
+        response = self.client.get("/staff/menu/")
+        self.assertEqual(response.status_code, 302)
+
+    def test_add_menu_page(self):
+        """Testing the add menu page url"""
+        response = self.client.get("/staff/menu/add/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_edit_menu_page(self):
+        """Testing the edit menu page url with a dummy ID"""
+        response = self.client.get("/staff/menu/edit/1/")
+        self.assertEqual(response.status_code, 404)
+
+    def test_delete_menu_page(self):
+        """Testing the delete menu page url with a dummy ID"""
+        response = self.client.get("/staff/menu/delete/1/")
+        self.assertEqual(response.status_code, 404)
+
+    def test_set_active_menu_page(self):
+        """Testing the set active menu page url with a dummy ID"""
+        response = self.client.get("/staff/menu/set-active/1/")
+        self.assertEqual(response.status_code, 404)
+
+    def test_add_menu_item_page(self):
+        """Testing the add menu item page url"""
+        response = self.client.get("/staff/menu-item/add/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_edit_menu_item_page(self):
+        """Testing the edit menu item page url with a dummy ID"""
+        response = self.client.get("/staff/menu-item/edit/1/")
+        self.assertEqual(response.status_code, 404)
+
+    def test_delete_menu_item_page(self):
+        """Testing the delete menu item page url with a dummy ID"""
+        response = self.client.get("/staff/menu-item/delete/1/")
+        self.assertEqual(response.status_code, 404)
+
+    def test_add_item_page(self):
+        """Testing the add item page url"""
+        response = self.client.get("/staff/item/add/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_edit_item_page(self):
+        """Testing the edit item page url with a dummy ID"""
+        response = self.client.get("/staff/item/edit/1/")
+        self.assertEqual(response.status_code, 404)
+
+    def test_delete_item_page(self):
+        """Testing the delete item page url with a dummy ID"""
+        response = self.client.get("/staff/item/delete/1/")
+        self.assertEqual(response.status_code, 404)
+
+    def test_no_access_page(self):
+        """Testing the no access page url"""
+        response = self.client.get("/no-access/")
+        self.assertEqual(response.status_code, 200)
