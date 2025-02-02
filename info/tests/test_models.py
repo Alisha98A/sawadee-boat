a14c335edc2a833b48916acee3e16d01f4ce1a41
Alisha <alishaanderssonn@icloud.com>
2025-02-02 21:12:00 +0100

    Add tests for MenuItem model

--- info/tests/test_models.py
+++ info/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from info.models import Menu
+from info.models import Menu, MenuItem, Item
 
 
 class TestMenuModel(TestCase):
@@ -22,3 +22,21 @@ class TestMenuModel(TestCase):
         menu = Menu.objects.get(name="Dinner Menu")
         self.assertEqual(menu.description, "A delicious dinner selection.")
         self.assertTrue(menu.is_active)
+
+
+class TestMenuItemModel(TestCase):
+    """Tests for the MenuItem model."""
+
+    def setUp(self):
+        """Set up test menu and menu items."""
+        self.menu = Menu.objects.create(name="Lunch Menu")
+        self.menu_item = MenuItem.objects.create(menu=self.menu, category="Appetizers")
+
+    def test_menu_item_str(self):
+        """Test the string representation of the MenuItem model."""
+        self.assertEqual(str(self.menu_item), "Lunch Menu - Appetizers")
+
+    def test_menu_item_creation(self):
+        """Test that a menu item can be created and retrieved."""
+        menu_item = MenuItem.objects.get(category="Appetizers")
+        self.assertEqual(menu_item.menu.name, "Lunch Menu")
