9266bb1ee35836fbc76a75e612bc271d891a0fed
Alisha <alishaanderssonn@icloud.com>
2025-02-02 23:00:01 +0100

    Install crispy forms and use it in password_change.html

--- sawadee_dining_boat/settings.py
+++ sawadee_dining_boat/settings.py
@@ -51,6 +51,8 @@ INSTALLED_APPS = [
     'cloudinary',
     'allauth',
     'allauth.account',
+    'crispy_forms',
+    'crispy_bootstrap5',
     
     'reservations',
     'info',
@@ -212,3 +214,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
