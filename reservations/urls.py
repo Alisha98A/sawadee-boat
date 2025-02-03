6c1d9f816a28930485e9b596cf610fd72008b9fe
Alisha <alishaanderssonn@icloud.com>
2025-02-03 18:50:39 +0100

    Delete success template from urls

--- reservations/urls.py
+++ reservations/urls.py
@@ -12,7 +12,6 @@ app_name = 'reservations'
 urlpatterns = [
     # Create reservation and success page
     path("reservations/create/", ReservationCreateView.as_view(), name="reservation_create"),
-    path('success/', views.reservation_success, name='reservation_success'),
 
     # List view, edit and delete
     path("reservations/", ReservationListView.as_view(), name="reservations_list"),
