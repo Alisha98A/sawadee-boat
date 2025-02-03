02d01403af38ffeeb9c0bf390e6f979e7f4e6978
Alisha <alishaanderssonn@icloud.com>
2025-02-03 21:09:46 +0100

    Fix PEP8 violations in Profile model

--- accounts/models.py
+++ accounts/models.py
@@ -3,8 +3,10 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
- #Create your models here.
+
+# Create your models here.
 class Profile(models.Model):
+    """Profile model to extend the default User model."""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=30, blank=True)
@@ -15,6 +17,7 @@ class Profile(models.Model):
     def __str__(self):
         return f"{self.user.username}'s Profile"
 
+
 # Ensure email is required in the User model
-User._meta.get_field('email').blank = False
-User._meta.get_field('email').null = False
+User._meta.get_field("email").blank = False
+User._meta.get_field("email").null = False
